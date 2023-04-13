6ef89e9d65f622d17b7e34c689423081ca4b580f
sainath cholkar <sainath.c@turtlemint.com>
2023-04-13 17:49:33 +0530

    PR Title Checker

--- src/src/main/java/org/example/Main.java
+++ src/src/main/java/org/example/Main.java
@@ -2,9 +2,7 @@ package org.example;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Creating a PR to test gitStream");
-    System.out.println("Creating Test commit");
-    System.out.println("Creating Test commit");
+
     System.out.println("Creating a PR to test gitStream");
 
     System.out.println("Creating a PR to test gitStream");
